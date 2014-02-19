dfd073c940bf8c4d81d17f96638224e1b2ee37d9
Jeff Chen <jeffrey@cmu.edu>
2014-02-19 12:44:35 -0500

    Start work on ratings

--- scripts/track.js
+++ scripts/track.js
@@ -7,10 +7,12 @@ var Track = {
 	current_time : '',
 	total_time : '',
 	status : '',
+	thumb_status: ''
 
 	now_playing : function() {
 		this.song_title = $("#playerSongTitle").text();
 		this.artist = $("#player-artist").text();
+		this.album = $('#player-album').text();
 		this.album_art = "http://" + $("#playingAlbumArt").attr('src');
 		this.current_time = $("#time_container_current").text();
 		this.total_time = $("#time_container_duration").text();
@@ -18,6 +20,15 @@ var Track = {
 		if ($('button[data-id="play-pause"]').hasClass('playing')) {
 			// console.log('this is playing');
 			status = 'Pause';
+		}
+		if ($('li[title="Thumbs up"]').hasClass('selected')) {
+			this.thumb_status = 'Up';
+		}
+		else if ($('li[title="Thumbs down"]').hasClass('selected')) {
+			this.thumb_status = 'Down';
+		}
+		else {
+			this.thumb_status = 'None';
 		}
 		// else {
 		// 	console.log('this is not playing');
